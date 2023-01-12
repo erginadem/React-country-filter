2da2a0522ecfe4840dc7642f747b055a2d4be60a
Ergin Adem <erginadem@Ergins-MacBook-Air.local>
2023-01-12 15:29:34 -0700

    V1-completed main tasks.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import CountryFilter from "./components/country-filter/CountryFilter";
+
+const App = () => {
+  return (
+    <div>
+      <CountryFilter />
+    </div>
+  );
+};
+
+export default App;
